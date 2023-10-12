398b77e1244c6a0579f49a58aef6d63ac049a2d8
livia araujo <liviaaraujo.dev@gmail.com>
2023-10-12 17:13:47 -0300

    create about

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import { Header } from "./components/Header";
 import React from "react";
 import { Home } from "./components/Home";
+import { About } from "./components/About";
 
 function App() {
 
-  const [darkToggle, setDarkToggle] = React.useState(false)
+  const [darkToggle, setDarkToggle] = React.useState(true)
 
   return (
     <div className={darkToggle ? 'dark' : 'light'} >
       <Header />
       <Home />
+      <About/>
     </div>
   )
 }
