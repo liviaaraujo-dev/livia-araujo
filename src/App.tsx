3883dc9c345a7d1ed032c4b46fb64f8f51b8b431
livia araujo <liviaaraujo.dev@gmail.com>
2023-10-13 21:24:11 -0300

    create projects

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { About } from "./components/About";
 import { Skills } from "./components/Skills";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
+import Projects from "./components/Projects";
 
 function App() {
 
@@ -16,6 +17,7 @@ function App() {
       <Home />
       <About />
       <Skills />
+      <Projects />
       <Contact />
       <Footer/>
     </div>
